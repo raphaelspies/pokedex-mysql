4870080d6ef57b34c86d1afa133cef0f0011f8cf
raphspies <raphspies@gmail.com>
2021-05-29 17:08:51 -0700

    Set up basic routing & react rendering

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
 // TODO: Create an express server
+const cors = require('cors')
+const express = require('express');
+const path = require('path')
+const app = express();
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`express listening on port ${port}`)
+})
+
+// app.use('/client', express.static(path.join(__dirname, 'dist')))
+// app.use(express.static(path.join(__dirname, 'dist')))
+
+app.use(express.static(path.join(__dirname,  '../client/dist')))
+
+// app.get('/', (req, res) => {
+//   res.send('hello, world')
+// })
